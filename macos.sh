6b9813feebd4a84c097a8b7880e77689d31a8e5a
Ankur Oberoi <aoberoi@gmail.com>
2021-04-11 18:53:35 -0700

    add macos defaults script

---
+++ macos.sh
@@ -0,0 +1,10 @@
+# Setup default settings for macOS
+
+[ `uname` != Darwin ] && echo "Skipping macOS default setup." && exit 0
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
